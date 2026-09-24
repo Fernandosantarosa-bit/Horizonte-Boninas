a72449df88d1c990287cbe88dfc82be1663e1fc8
Fernandosantarosa-bit <fernandosantarosa40@gmail.com>
2026-09-24 10:59:52 +0000

    feat: initialize src/lib/supabase.ts

---
+++ src/lib/supabase.ts
@@ -0,0 +1,1 @@
+import {createClient} from "@supabase/supabase-js";const url=import.meta.env.VITE_SUPABASE_URL||"https://whkkurtdiaztiexhjtlz.supabase.co";const key=import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY;export const supabase=key?createClient(url,key):null;
